66ee09c368760929c071313d2618407d305ee03f
Ravi Ture <raviture@gmail.com>
2014-11-21 12:58:58 +0530

    Overridden the ActiveSupport::TimeWithZone's 'method_missing' method.

--- lib/formatted_times.rb
+++ lib/formatted_times.rb
@@ -1,100 +1,106 @@
 require "formatted_times/version"
 
-module FormattedTimes
-  formatting_options = {
+module ActiveSupport
+  class TimeWithZone
 
-    # Date related options
+    FORMATTING_OPTIONS = {
 
-    'YY' => '%Y',
-    'CC' => '%C',
-    'yy' => '%y',
-    'mm' => '%m',
-    'BB' => '%B',
-    'bb' => '%b',
-    'hh' => '%h',
-    'dd' => '%d',
-    'ee' => '%e',
-    'jj' => '%j',
+      # Date related options
 
-    # Time related options
+      'YY' => '%Y',
+      'CC' => '%C',
+      'yy' => '%y',
+      'mm' => '%m',
+      'BB' => '%B',
+      'bb' => '%b',
+      'hh' => '%h',
+      'dd' => '%d',
+      'ee' => '%e',
+      'jj' => '%j',
 
-    'HH' => '%H',
-    'kk' => '%k',
-    'II' => '%I',
-    'll' => '%l',
-    'PP' => '%P',
-    'pp' => '%p',
-    'MM' => '%M',
-    'SS' => '%S',
-    'LL' => '%L',
-    'NN' => '%N',
-    '3N' => '%3N',
-    '6N' => '%6N',
-    '9N' => '%9N',
-    '12N' => '%12N',
+      # Time related options
 
-    # Time zone related Options
+      'HH' => '%H',
+      'kk' => '%k',
+      'II' => '%I',
+      'll' => '%l',
+      'PP' => '%P',
+      'pp' => '%p',
+      'MM' => '%M',
+      'SS' => '%S',
+      'LL' => '%L',
+      'NN' => '%N',
+      '3N' => '%3N',
+      '6N' => '%6N',
+      '9N' => '%9N',
+      '12N' => '%12N',
 
-    'zz' => '%z',
-    '1z' => '%:z',
-    '2z' => '%::z',
-    '3z' => '%:::z',
-    'ZZ' => '%Z',
+      # Time zone related Options
 
-    # Weekday related options
+      'zz' => '%z',
+      '1z' => '%:z',
+      '2z' => '%::z',
+      '3z' => '%:::z',
+      'ZZ' => '%Z',
 
-    'AA' => '%A',
-    'aa' => '%a',
-    'uu' => '%u',
-    'ww' => '%w',
+      # Weekday related options
 
-    # Seconds related opions
+      'AA' => '%A',
+      'aa' => '%a',
+      'uu' => '%u',
+      'ww' => '%w',
 
-    'ss' => '%s',
-    'QQ' => '%Q',
+      # Seconds related opions
 
-    # Literal string related options
+      'ss' => '%s',
+      'QQ' => '%Q',
 
-    'nn' => '%n',
-    'tt' => '%t',
+      # Literal string related options
 
-    # Combination Options
+      'nn' => '%n',
+      'tt' => '%t',
 
-    'cc' => '%c',
-    'DD' => '%D',
-    'FF' => '%F',
-    'vv' => '%v',
-    'xx' => '%x',
-    'XX' => '%X',
-    'rr' => '%r',
-    'RR' => '%R',
-    'TT' => '%T'
-  }
+      # Combination Options
+
+      'cc' => '%c',
+      'DD' => '%D',
+      'FF' => '%F',
+      'vv' => '%v',
+      'xx' => '%x',
+      'XX' => '%X',
+      'rr' => '%r',
+      'RR' => '%R',
+      'TT' => '%T'
+    }
 
 
-  def method_missing(sym, *args, &block)
-    puts sym.inspect, args.inspect, self.inspect, '------------------'
-    method_name = sym.to_s
-    if method_name.starts_with? 'frmt_'
-      strf_time = get_strftime_string(method_name)
-    else
-      super
+    def method_missing(sym, *args, &block)
+      method_name = sym.to_s
+
+      if method_name.starts_with? 'frmt_'
+        if args.length == 1 and args[0].is_a?(String)
+          strf_time_string = get_strftime_string(method_name, args[0])
+        else
+          strf_time_string = get_strftime_string(method_name)
+        end
+        return self.strftime(strf_time_string)
+      end
+
+      wrap_with_time_zone time.__send__(sym, *args, &block)
+    rescue NoMethodError => e
+      raise e, e.message.sub(time.inspect, self.inspect), e.backtrace
     end
 
-  end
+    def get_strftime_string(name, separator=' : ')
+      options = name.split('_')
+      options.shift
+      invalid_options = options - FORMATTING_OPTIONS.keys
 
-  def get_strftime_string(name)
-    options = name.split('_')
-    invalid_options = options - formatting_options.keys
-    if invalid_options.empty?
-      options.inject(''){ |strf_string, option|
-        strf_string << formatting_options[option]
-      }
-    else
-      raise ArgumentError, "Options #{invalid_options.join(', ')} are invalid."
+      raise ::ArgumentError, "Options #{invalid_options.join(', ')} are invalid." unless invalid_options.empty?
+
+      options.collect{ |option| FORMATTING_OPTIONS[option] }.join(separator)
+
     end
+
   end
 end
-
-
-ActiveSupport::TimeWithZone.send :include, FormattedTimes
